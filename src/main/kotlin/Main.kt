cb65db69a25726596e9f9e3bcb1e34fb54ddf216
TeterinMN <Scorpion.TMN@ya.ru>
2022-07-05 20:47:26 +0700

    Создал файл Main.kt, добавил код

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,41 @@
+const val day = 24 * (60 * 60)
+const val hour = 60 * 60
+const val min = 60
+
+fun main() {
+
+    println("Был(а) в сети ${agoToText(54)}")
+    println("Был(а) в сети ${agoToText(21 * min)}")
+    println("Был(а) в сети ${agoToText(21 * hour)}")
+    println("Был(а) в сети ${agoToText(3 * day)}")
+    println("Был(а) в сети ${agoToText(2 * day)}")
+    println("Был(а) в сети ${agoToText(8 * day)}")
+
+}
+
+
+fun agoToText(time: Int): String {
+    val text: String = when {
+        time in 1..59 -> "только что"
+        time in min..hour -> "${time / 60} ${minut(time / 60)} назад"
+        time >= hour + 1 && time <= day -> "${time / (60 * 60)} ${hours(time / (60 * 60))} назад"
+        time <= 2 * day && time > 1 * day -> "сегодня"
+        time <= 3 * day && time > 2 * day -> "вчера"
+        else -> "давно"
+    }
+    return text
+}
+
+
+fun hours(ho: Int) = when (ho % 10) {
+    1 -> if (ho == 11) "часов" else "час"
+    2, 3, 4 -> "часа"
+    else -> "часов"
+}
+
+fun minut(mi: Int) = when (mi % 10) {
+    1 -> if (mi == 11) "минут" else "минуту"
+    2, 3, 4 -> "минуты"
+    else -> "минут"
+}
+
