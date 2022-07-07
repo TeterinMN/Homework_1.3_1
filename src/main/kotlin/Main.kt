74e2433bc759a710c79f4b6624fb105bd477c06e
TeterinMN <Scorpion.TMN@ya.ru>
2022-07-07 21:49:37 +0700

    Доработка

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,7 +5,7 @@ const val min = 60
 fun main() {
 
     println("Был(а) в сети ${agoToText(54)}")
-    println("Был(а) в сети ${agoToText(56 * min)}")
+    println("Был(а) в сети ${agoToText(11 * min)}")
     println("Был(а) в сети ${agoToText(21 * hour)}")
     println("Был(а) в сети ${agoToText(3 * day)}")
     println("Был(а) в сети ${agoToText(2 * day)}")
@@ -28,14 +28,12 @@ fun agoToText(time: Int): String {
 
 
 fun hours(ho: Int) = when (ho) {
-    11 -> "часов"
     1, 21 -> "час"
     2, 3, 4, 22, 23, 24 -> "часа"
     else -> "часов"
 }
 
 fun minut(mi: Int) = when (mi) {
-    11 -> "минут"
     1, 21, 31, 41, 51 -> "минуту"
     2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54 -> "минуты"
     else -> "минут"
